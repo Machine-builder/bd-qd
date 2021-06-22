fe52c7a5b69de9b68c597e8143ebe70ea48e53d6
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2021-06-22 20:07:59 +1000

    Add files via upload

---
+++ sub_modules.js
@@ -0,0 +1,33 @@
+var buying_options = [
+    {
+        "name": "JSON Templating",
+        "description": `This module allows for you to reference base
+json files, which in turn speeds up development drastically,
+since you'll no longer need to program the basics`,
+        "price": 0.45,
+        "toggled": false
+    },
+    {
+        "name": "Item & Block Templates",
+        "description": `This module adds prebuilt item & block templates,
+simplifying the process of creating a bunch of different
+types of items, and a variety of blocks`,
+        "price": 0.6,
+        "toggled": false
+    },
+    {
+        "name": "FRG Tools",
+        "description": `This module provides quick access to premade code samples,
+which you can reference easily within your own files`,
+        "price": 1.45,
+        "toggled": false
+    },
+    {
+        "name": "NPC Builder",
+        "description": `This module proves a tool for quick development
+of NPCS & other state-based entities,
+perfect for adventure maps & mini-games`,
+        "price": 1.55,
+        "toggled": false
+    }
+]
