016cbeefc68c81f5bc97732802c4f1bbb9623919
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2021-06-26 19:01:42 +1000

    Add files via upload

--- buying.js
+++ buying.js
@@ -34,8 +34,8 @@ window.onload = function() {
 
         var child_html = `\
             <div class="col-md-6 no-select" id="buying-option-`+String(i)+`">
-                <div class="pack-preview d-flex flex-row" onclick="toggle_buying_option(`+String(i)+`)">
-                    <div class="p-2">
+                <div class="pack-preview d-flex flex-row">
+                    <div class="p-2" onclick="toggle_buying_option(`+String(i)+`)">
                         <h2 class="pack-title"><span class="module-price">[USD$`+strPrice+`]</span> `+name+`</h2>
                         <p class="pack-description">
                         `+desc+`
@@ -43,7 +43,8 @@ window.onload = function() {
                     </div>
                     
                     <div class="pack-configure-cog p-2">
-                        <i class="fa fa-toggle-off" aria-hidden="true"></i>
+                        <i onclick="clicked_info_button(`+String(i)+`)" class="fa fa-info-circle" aria-hidden="true"></i>
+                        <i onclick="toggle_buying_option(`+String(i)+`)" class="fa fa-toggle-off" aria-hidden="true"></i>
                     </div>
                 </div>
             </div>`;
@@ -52,6 +53,86 @@ window.onload = function() {
     };
 
     refresh_purchase_code();
+}
+
+var toggled_info_box = undefined;
+var sliding_animation = false;
+
+function div_slide_up() {
+    sliding_animation = true;
+    var dropdown_div = document.getElementById('module-info-dropdown-div');
+    dropdown_div.style.animation = "slide_div_up 0.7s";
+    dropdown_div.style.transform = "translate(-50%, -110%)";
+    window.setTimeout(function() {
+        sliding_animation = false;
+    }, 700)
+}
+function div_slide_down() {
+    sliding_animation = true;
+    var dropdown_div = document.getElementById('module-info-dropdown-div');
+    dropdown_div.style.animation = "slide_div_down 0.7s";
+    dropdown_div.style.transform = "translate(-50%, 15px)";
+    window.setTimeout(function() {
+        sliding_animation = false;
+    }, 700)
+}
+
+function hide_info_box() {
+    toggled_info_box = undefined;
+    div_slide_up();
+}
+
+function reload_dropdown_div() {
+    var module_info = buying_options[toggled_info_box];
+    var module_name = module_info['name'];
+    var module_desc = module_info['description'];
+    var module_price = module_info['price'];
+    var module_desc_long = module_info['long_description'];
+
+    var dropdown_html = `
+        <div class="pack-configure-cog white p-2" onclick="hide_info_box()">
+            <i class="fa fa-window-close" aria-hidden="true"></i>
+        </div>
+
+        <div class="title-icon p-2 container" style="margin-left:12px;">
+            <img src="./icons/256x256_transparent.png" class="title-icon"></img>
+        </div>
+        <h2 class="pack-title">
+            <span class="module-price dark">USD$`+stringifyPrice(module_price)+`</span>
+            <br>
+            `+module_name+`</h2>
+        <p class="pack-description">
+        `+module_desc+`
+        </p>
+        
+        <span class="pack-description-long">`+module_desc_long+`</span>
+        `;
+    
+    var dropdown_div = document.getElementById('module-info-dropdown-div');
+    dropdown_div.innerHTML = dropdown_html
+}
+
+function clicked_info_button(index) {
+
+    if (sliding_animation) {
+        return
+    }
+
+    if (toggled_info_box === undefined) {
+        toggled_info_box = index;
+        reload_dropdown_div();
+        div_slide_down();
+    } else if (toggled_info_box != index) {
+        toggled_info_box = index;
+        div_slide_up();
+        window.setTimeout(function() {
+            reload_dropdown_div();
+            div_slide_down();
+        }, 600)
+    } else {
+        toggled_info_box = undefined;
+        div_slide_up();
+    }
 }
 
 function toggle_buying_option(index) {
@@ -64,9 +145,7 @@ function toggle_buying_option(index) {
 
     var elem_id = "buying-option-"+String(index);
     var buying_option_elem = document.getElementById(elem_id);
-    var icon_elem = buying_option_elem.children[0].children[1].children[0];
-
-    console.log(toggled);
+    var icon_elem = buying_option_elem.children[0].children[1].children[1];
     
     if (toggled) {
         icon_elem.classList.remove('fa-toggle-off');
@@ -90,31 +169,29 @@ function toggle_buying_option(index) {
     };
 
     total_price = Math.round(total_price*100)/100;
-    var total_price_str = stringifyPrice(total_price);
+    var total_price_str = "USD$"+stringifyPrice(total_price);
 
     var order_sum = document.getElementById('order-summary');
-    order_sum.innerHTML = `
-    You have selected `+String(selected_options)+`/4 options
-    <br>
-    This will cost you USD$`+total_price_str+`
-    `
+    order_sum.innerHTML = `You have selected `+String(selected_options)+`/4 options`;
+    document.getElementById("total-cost").innerText = total_price_str
 
     refresh_purchase_code();
 }
 
 function refresh_purchase_code() {
-    var purchase_code = "";
+    var purchase_info = {
+        "module_uids": []
+    }
+
     for (var i=0; i<buying_options.length; i++) {
         var option = buying_options[i];
         var toggled = option['toggled'];
         if (toggled) {
-            purchase_code += '1';
-        } else {
-            purchase_code += '0';
+            purchase_info['module_uids'].push(option['uid']);
         }
     };
 
-    purchase_code += '-';
+    purchase_code = JSON.stringify(purchase_info);
 
     var purchase_code_b64 = btoa(purchase_code);
     document.getElementById('purchase-code').innerText = purchase_code_b64;
--- sub_modules.js
+++ sub_modules.js
@@ -4,30 +4,38 @@ var buying_options = [
         "description": `This module allows for you to reference base
 json files, which in turn speeds up development drastically,
 since you'll no longer need to program the basics`,
-        "price": 0.45,
-        "toggled": false
+        "long_description": "            <p>\n                This allows you to create \"base\" or \"template\" files,\n                which you can reference freely from other files in your\n                behavior pack to build from. This, in turn, gives you the\n                freedom to write far fewer lines of code for each item,\n                block, entity or any other json files you write.\n            </p>\n        ",
+        "price": 0.8,
+        "toggled": false,
+        "uid": "ahwo"
     },
     {
         "name": "Item & Block Templates",
         "description": `This module adds prebuilt item & block templates,
 simplifying the process of creating a bunch of different
 types of items, and a variety of blocks`,
-        "price": 0.6,
-        "toggled": false
+        "long_description": "            <p>\n                This module is basically JSON templating, on steriods, for items & blocks.\n                The main idea of this module is that it can be used to simplify\n                the process of creating custom items & blocks greatly. There are\n                now new simple structures that you can use to generate\n                fully-functional item & block files from.\n            </p>\n        ",
+        "price": 1.2,
+        "toggled": false,
+        "uid": "akfn"
     },
     {
         "name": "FRG Tools",
         "description": `This module provides quick access to premade code samples,
 which you can reference easily within your own files`,
-        "price": 1.45,
-        "toggled": false
+        "long_description": "            <p>\n                Behold, the beautiful FRG, now stripped down to its most basic form.\n                This module provides quick access to a lot of different structure\n                generation templates, which greatly reduces the effort you need\n                to put in to making randomly generating structures in your\n                behavior packs.\n            </p>\n        ",
+        "price": 3,
+        "toggled": false,
+        "uid": "vowb"
     },
     {
         "name": "NPC Builder",
         "description": `This module proves a tool for quick development
 of NPCS & other state-based entities,
 perfect for adventure maps & mini-games`,
-        "price": 1.55,
-        "toggled": false
+        "long_description": "            <p>\n                This is the one, all powerful. This module allows you to create\n                state-based sequences for entities, which is perfect for creating\n                NPCs for a story game, or even just basic custom entities. The system\n                is versatile and easy to get started with.\n            </p>\n        ",
+        "price": 4,
+        "toggled": false,
+        "uid": "pwny"
     }
 ]
